94d2e0ce75b8aa500fa1b757a6b2372054a17be1
Juwairia Baig <juwairiabaig2003@gmail.com>
2024-11-25 15:14:58 +0530

    upload onto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Updated import
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Get the root element from the DOM
+const rootElement = document.getElementById('root');
+
+// Create a React root and render the App component
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
